0ea17920e5e863015a4ea8efca4601ac2087c108
Flavia <flavia.brogle@students.fhnw.ch>
2021-01-06 11:36:21 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,63 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[][] field = {
+                {0, 0, 0,  9, 0, 0,  6, 0, 0, },
+                {0, 7, 0,  8, 1, 0,  5, 3, 0, },
+                {5, 1, 4,  2, 0, 0,  8, 0, 0, },
+                
+                {0, 0, 0,  0, 9, 0,  4, 2, 8, },
+                {0, 3, 0,  0, 0, 0,  0, 6, 0, },
+                {9, 4, 2,  0, 7, 0,  0, 0, 0, },
+                
+                {0, 0, 6,  0, 0, 9,  3, 1, 5, },
+                {0, 9, 8,  0, 6, 5,  0, 7, 0, },
+                {0, 0, 7,  0, 0, 3,  0, 0, 0, },
+                
+        };
+        SudokuField sudokuField = new SudokuField(field, true);
+        System.out.println("START:");
+        System.out.println(sudokuField);
+        
+        solveSudokuRecursive(sudokuField);
+    
+        System.out.println("END:");
+        System.out.println(sudokuField);
+    }
+    
+    private static boolean solved = false;
+    public static void solveSudokuRecursive(SudokuField sudokuField) {
+        Position firstEmptyPosition = null;
+        outerloop:
+        for (int x = 0; x < sudokuField.getSize(); x++) {
+            for (int y = 0; y < sudokuField.getSize(); y++) {
+                if (sudokuField.getValue(x, y) == SudokuField.EMPTY_VALUE) {
+                    firstEmptyPosition = new Position(x, y);
+                    break outerloop;
+                }
+            }
+        }
+        if (firstEmptyPosition == null) {
+            solved = true;
+            System.out.println("SOLVED :D");
+            return; // it's finished :D
+        }
+        
+        for (int i = 1; i <= 9; i++) {
+            if (!sudokuField.isValueValid(i, firstEmptyPosition.x, firstEmptyPosition.y, true)) {
+                continue;
+            }
+            sudokuField.setValue(i, firstEmptyPosition);
+            solveSudokuRecursive(sudokuField);
+            if (solved) {
+                System.out.println("already solved");
+                return;
+            }
+        }
+        if (sudokuField.getValue(firstEmptyPosition) == SudokuField.EMPTY_VALUE) {
+            System.out.println("No Valid position found at Position " + firstEmptyPosition);
+            System.out.println(sudokuField);
+        }
+        sudokuField.setValue(SudokuField.EMPTY_VALUE, firstEmptyPosition);
+    }
+}
---
+++ src/SudokuField.java
@@ -0,0 +1,114 @@
+public class SudokuField {
+    
+    public static int EMPTY_VALUE = 0;
+    
+    private final int size = 9;
+    
+    private final int[][] field;
+    
+    public SudokuField() {
+        field = new int[size][];
+        for (int i = 0; i < size; i++) {
+            field[i] = new int[size];
+            for (int j = 0; j < size; j++) {
+                this.field[i][j] = EMPTY_VALUE;
+            }
+        }
+    }
+    
+    public SudokuField(int[][] otherField, boolean switchXY) {
+        field = new int[size][];
+        if (switchXY) {
+            for (int x = 0; x < size; x++) {
+                field[x] = new int[size];
+                for (int y = 0; y < size; y++) {
+                    field[x][y] = otherField[y][x];
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                field[i] = new int[size];
+                System.arraycopy(otherField[i], 0, this.field[i], 0, size);
+            }
+        }
+    }
+    
+    public SudokuField createCopy() {
+        return new SudokuField(this.field, false);
+    }
+    
+    
+    public boolean isValueValid(int value, int x, int y, boolean ignoreValuePresent) {
+        if (getValue(x, y) != EMPTY_VALUE && !ignoreValuePresent) {
+            System.out.println(value + " invalid FULL at " + new Position(x, y));
+            return false;
+        }
+        for (int i = 0; i < size; i++) {
+            if (
+                    getValue(x, i) == value
+                    || getValue(i, y) == value
+            ) {
+                System.out.println(value + " invalid LINE at " + new Position(x, y));
+                return false;
+            }
+        }
+        Position fieldStart = getSmallFieldTopLeftPosition(new Position(x, y));
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (getValue(fieldStart.x + i, fieldStart.y + j) == value) {
+                    System.out.println(value + " invalid BOX  at " + new Position(x, y));
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    public Position getSmallFieldTopLeftPosition(Position position) {
+        return new Position((position.x / 3) * 3, (position.y / 3) * 3);
+    }
+    
+    
+    public int getSize() {
+        return this.size;
+    }
+    
+    public int getValue(int x, int y) {
+        return field[x][y];
+    }
+    
+    public void setValue(int value, int x, int y) {
+        field[x][y] = value;
+    }
+    
+    public void setValue(int value, Position position) {
+        setValue(value, position.x, position.y);
+        System.out.println(position + ":= " + value);
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder out = new StringBuilder();
+        for (int y = 0; y < size; y++) {
+            if (y % 3 == 0) {
+                for (int i = 0; i < (size*3 + 4); i++) {
+                    out.append("-");
+                }
+                out.append("\n");
+            }
+            for (int x = 0; x < size; x++) {
+                if (x % 3 == 0) out.append("|");
+                out.append(" " + (getValue(x, y) == EMPTY_VALUE ? " " : getValue(x, y)) + " ");
+            }
+            out.append("|\n");
+        }
+        for (int i = 0; i < (size*3 + 4); i++) {
+            out.append("-");
+        }
+        return out.toString();
+    }
+    
+    public int getValue(Position position) {
+        return getValue(position.x, position.y);
+    }
+}
